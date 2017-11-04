734a8b1a893eb39f06e87557b4165393e5348b31
Josh McCormick <jmccormick1024@gmail.com>
2017-11-04 10:37:59 -0400

    starter class for methods to grab various social media shares

---
+++ plugins/social-media-tracker/social-media-class.php
@@ -0,0 +1,22 @@
+<?php 
+
+class socialMedia {
+    public function generate_email_report(){
+        
+    }
+    public function generate_csv_report(){
+
+    }
+    public function generate_sms_report(){
+
+    }
+    public function grab_twitter_activity(){
+
+    }
+    public function grab_facebook_data(){
+
+    }
+    public function grab_instagram_data(){
+
+    }
+}
