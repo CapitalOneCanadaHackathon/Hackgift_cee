6620c7a387f94d01c6a8222e8d25a5b36e8d3a17
Sierra Katrian <sierra@sierrakatrian.com>
2017-11-05 12:07:32 -0500

    Can Enter NOTES! Updated Select Option

--- plugins/social-media-tracker/includes/enter-info.php
+++ plugins/social-media-tracker/includes/enter-info.php
@@ -1,16 +1,24 @@
 <h1>Enter the Info for your student here.</h1>
-<?php function d($value){
-    if (is_array($value)){
-        echo '<pre>' .var_dump($value); echo '</pre>';die();
-    }
-    if (is_object($value)){
-       echo '<pre>'. print_r($value); echo '</pre>'; die();
-    }
-    else{
-        var_dump($value);die();
-    }
-}?>
-<?php $user = wp_get_current_user();
+<?php
+function d($obj) {
+	echo '<pre>';
+	print_r($obj);
+	echo '</pre>';
+}
+
+//  function d($value){
+//     if (is_array($value)){
+//         echo '<pre>' .var_dump($value); echo '</pre>';die();
+//     }
+//     if (is_object($value)){
+//        echo '<pre>'. print_r($value); echo '</pre>'; die();
+//     }
+//     else{
+//         var_dump($value);die();
+//     }
+// }
+
+$user = wp_get_current_user();
 
 $args = array(
     'post_type' => 'program_partner',
@@ -28,34 +36,88 @@ $args = array(
     )
         );
 $usersPrograms = new WP_Query($args);
-$theProgramID = $usersPrograms->posts[0]->ID;
+$theProgram = $usersPrograms->posts[0];
+$theProgramID = $theProgram->ID;
+// d($theProgramID);
+
 
 $studentArgs = array (
-    'post_type' => 'cee_members', 
-    'meta_query' => array (
-        array(
-            'program_%_name' => $theProgramID
-            )
-    )
+    'post_type' => 'cee_members',
 );
 
 $students = new WP_Query($studentArgs);
 $students = $students->posts;
+
 $studentsSelectBox = [];
 $counter = 0;
 foreach($students as $student){
-    $studentsSelectBox[$counter]['value'] = $student->ID;
-    $studentsSelectBox[$counter]['text'] = $student->post_title;
-    $counter ++;
+	// d($student);
+
+	$programs = get_field('program', $student->ID);
+
+	// check if the repeater field has rows of data
+	if($programs) :
+		if( have_rows('program', $student->ID) ):
+			while( have_rows('program', $student->ID) ): the_row();
+				$value = get_sub_field('name');
+				// d($value->ID);
+		        if ($value->ID == $theProgramID) :
+					echo 'works <br />';
+
+					$studentsSelectBox[$counter]['value'] = $student->ID;
+					$studentsSelectBox[$counter]['text'] = $student->post_title;
+					$counter ++;
+		        endif;
+			endwhile;
+		endif;
+	endif;
 }
 ?>
 
+<form class="" action="" method="post">
+	<select name="student">
+	    <?php foreach ($studentsSelectBox as $option) :?>
+	        <option value="<?php echo $option['value']?>"><?php echo $option['text'];?></option>
+	<?php endforeach;?>
+	</select>
+	<textarea class="notes" name="notes"></textarea>
+	<button class="submit" type="submit" name="submit">Submit</button>
+</form>
 
-<select name="student">
-    <?php foreach ($studentsSelectBox as $option) :?>
-        <option value="<?php echo $option['value']?>"><?php echo $option['text'];?></option>
-<?php endforeach;?>    
-</select>
-<textarea class="notes">
-</textarea>
-<button class="submit">Submit</button>
+<?php
+
+if(isset($_POST['submit'])):
+	// d($_POST['student']);
+echo 'Thank you for submitting your notes.';
+
+if ( have_rows('program', $_POST['student'])) :
+	$program = get_field('program', $_POST['student']);
+	for ($i=0; $i < count(get_field('program', $_POST['student'])) ; $i++) :
+		$programName = $program[$i]['name'];
+		if ($programName != '') :
+			if ($programName->ID == $theProgramID && $program[$i]['status'] == 'current') :
+				$today = date('d/m/Y');
+				// $row_id = add_row('field_59fe39149c2e2', $row, $students[0]->ID);
+				// echo $row_id;
+
+				//FIND OUT IF IT IS AN ARRAY ALREADY OR NOT
+	            if( !is_array($program[$i]['notes']) ) {  //WE DONT HAVE ANY NOTES
+	                $program[$i]['notes'] = array();
+	            }
+	            $addRow = array( //SET YOUR NEW ROW HERE
+					'date_taken' => $today,
+	                'note' => $_POST['notes'],
+	            );
+	            //ADD TO ARRAY
+	            array_push($program[$i]['notes'], $addRow);
+
+				$field_key = "field_59fe37e312391"; //FIELD KEY OF PARENT REPEATER 'media'
+				update_field($field_key, $program, $_POST['student']);
+			endif;
+		endif;
+	endfor;
+endif;
+
+endif;
+
+?>
--- plugins/social-media-tracker/includes/generate-program-partner-input-button.php
+++ plugins/social-media-tracker/includes/generate-program-partner-input-button.php
@@ -1,17 +1,17 @@
-<? php
+<?php
 add_action('admin_menu', 'generate_partner_button');
 
 function generate_partner_button(){
     global $current_user; // Use global
-    get_currentuserinfo(); // Make sure global is set, if not set it.
+    wp_get_current_user(); // Make sure global is set, if not set it.
     if( user_can( $current_user, "subscriber" ) && !user_can($current_user, '"administrator')){
         add_menu_page('Enter Data', 'Enter Data', 'subscriber', 'Enter Data', 'generate_page');
-        
+
     }
     function generate_page (){
         require_once 'enter-info.php';
-        
+
     }
 
-    
+
 }
