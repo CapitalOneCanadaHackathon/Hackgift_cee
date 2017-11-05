8636fae39c67e3855c9661e3016ae1b7e24278f6
Sierra Katrian <sierra@sierrakatrian.com>
2017-11-05 12:39:16 -0500

    Fixed form bugs

--- plugins/social-media-tracker/includes/enter-info.php
+++ plugins/social-media-tracker/includes/enter-info.php
@@ -27,96 +27,109 @@ $args = array(
     )
         );
 $usersPrograms = new WP_Query($args);
-$theProgram = $usersPrograms->posts[0];
-$theProgramID = $theProgram->ID;
-// d($theProgramID);
 
+// d($usersPrograms);
 
-$studentArgs = array (
-    'post_type' => 'cee_members',
-);
+if ($usersPrograms->posts) :
 
-$students = new WP_Query($studentArgs);
-$students = $students->posts;
+	$theProgram = $usersPrograms->posts[0];
+	$theProgramID = $theProgram->ID;
 
-$studentsSelectBox = [];
-$counter = 0;
-foreach($students as $student){
-	// d($student);
+		// d($theProgram);
 
-	$programs = get_field('program', $student->ID);
+	$studentArgs = array (
+	    'post_type' => 'cee_members',
+	);
 
-	// check if the repeater field has rows of data
-	if($programs) :
-		if( have_rows('program', $student->ID) ):
-			while( have_rows('program', $student->ID) ): the_row();
-				$value = get_sub_field('name');
-				// d($value->ID);
-		        if ($value->ID == $theProgramID) :
-					echo 'works <br />';
+	$students = new WP_Query($studentArgs);
+	$students = $students->posts;
 
-					$studentsSelectBox[$counter]['value'] = $student->ID;
-					$studentsSelectBox[$counter]['text'] = $student->post_title;
-					$counter ++;
-		        endif;
-			endwhile;
-		endif;
-	endif;
-}
-?>
+	$studentsSelectBox = [];
+	$counter = 0;
+	foreach($students as $student){
+		// d($student);
 
-<form class="" action="" method="post">
-	<h1>Enter the Info for your CEE member here.</h1>
-	<div class="info__err">
-	    <span name="errMsg"></span>
-	</div>
-	<div class="info__listArea"]>
-		<select name="student" class="info__listArea--select">
-		    <?php foreach ($studentsSelectBox as $option) :?>
-		        <option value="<?php echo $option['value']?>"><?php echo $option['text'];?></option>
-		<?php endforeach;?>
-		</select>
-	</div>
-	<div class="info__inputArea">
-		<label for="note">Note</label><br />
-		<textarea class="notes info__inputArea--text" name="note" rows="10" name="notes"></textarea>
-		<button class="submit button-primary info__btn" type="submit" name="submit">Submit</button>
-	</div>
-</form>
+		$programs = get_field('program', $student->ID);
 
-<?php
+		// check if the repeater field has rows of data
+		if($programs) :
+			if( have_rows('program', $student->ID) ):
+				while( have_rows('program', $student->ID) ): the_row();
+					$value = get_sub_field('name');
+					// d($value->ID);
+			        if ($value->ID == $theProgramID) :
 
-if(isset($_POST['submit'])):
-	// d($_POST['student']);
-echo 'Thank you for submitting your notes.';
-
-if ( have_rows('program', $_POST['student'])) :
-	$program = get_field('program', $_POST['student']);
-	for ($i=0; $i < count(get_field('program', $_POST['student'])) ; $i++) :
-		$programName = $program[$i]['name'];
-		if ($programName != '') :
-			if ($programName->ID == $theProgramID && $program[$i]['status'] == 'current') :
-				$today = date('d/m/Y');
-				// $row_id = add_row('field_59fe39149c2e2', $row, $students[0]->ID);
-				// echo $row_id;
-
-				//FIND OUT IF IT IS AN ARRAY ALREADY OR NOT
-	            if( !is_array($program[$i]['notes']) ) {  //WE DONT HAVE ANY NOTES
-	                $program[$i]['notes'] = array();
-	            }
-	            $addRow = array( //SET YOUR NEW ROW HERE
-					'date_taken' => $today,
-	                'note' => $_POST['notes'],
-	            );
-	            //ADD TO ARRAY
-	            array_push($program[$i]['notes'], $addRow);
-
-				$field_key = "field_59fe37e312391"; //FIELD KEY OF PARENT REPEATER 'media'
-				update_field($field_key, $program, $_POST['student']);
+						$studentsSelectBox[$counter]['value'] = $student->ID;
+						$studentsSelectBox[$counter]['text'] = $student->post_title;
+						$counter ++;
+			        endif;
+				endwhile;
 			endif;
 		endif;
-	endfor;
-endif;
+	}
+
+
+
+	?>
+
+	<form class="" action="" method="post">
+		<h1>Enter the Info for your CEE member here.</h1>
+		<div class="info__err">
+		    <span name="errMsg"></span>
+		</div>
+		<div class="info__listArea"]>
+			<select name="student" class="info__listArea--select cee_member">
+			    <?php foreach ($studentsSelectBox as $option) :?>
+			        <option value="<?php echo $option['value']?>"><?php echo $option['text'];?></option>
+			<?php endforeach;?>
+			</select>
+		</div>
+		<div class="info__inputArea">
+			<label for="notes">Note</label><br />
+			<textarea class="cee_notes notes info__inputArea--text" rows="10" name="notes"></textarea>
+			<button class="submit button-primary info__btn" type="submit" name="submit">Submit</button>
+		</div>
+	</form>
+
+	<?php
+
+	if(isset($_POST['submit'])):
+		// d($_POST['student']);
+	echo 'Thank you for submitting your notes.';
+
+	if ( have_rows('program', $_POST['student'])) :
+		$program = get_field('program', $_POST['student']);
+		for ($i=0; $i < count(get_field('program', $_POST['student'])) ; $i++) :
+			$programName = $program[$i]['name'];
+			if ($programName != '') :
+				if ($programName->ID == $theProgramID && $program[$i]['status'] == 'current') :
+					$today = date('d/m/Y');
+					// $row_id = add_row('field_59fe39149c2e2', $row, $students[0]->ID);
+					// echo $row_id;
+
+					//FIND OUT IF IT IS AN ARRAY ALREADY OR NOT
+		            if( !is_array($program[$i]['notes']) ) {  //WE DONT HAVE ANY NOTES
+		                $program[$i]['notes'] = array();
+		            }
+		            $addRow = array( //SET YOUR NEW ROW HERE
+						'date_taken' => $today,
+		                'note' => $_POST['notes'],
+		            );
+		            //ADD TO ARRAY
+		            array_push($program[$i]['notes'], $addRow);
+
+					$field_key = "field_59fe37e312391"; //FIELD KEY OF PARENT REPEATER 'media'
+					update_field($field_key, $program, $_POST['student']);
+					endif;
+				endif;
+			endfor;
+		endif;
+
+	endif;
+
+else :
+
+	echo '<h1>You don\'t have any members to update.</h1>';
 
 endif;
 
--- plugins/social-media-tracker/social-media-tracker.php
+++ plugins/social-media-tracker/social-media-tracker.php
@@ -1,13 +1,13 @@
 
 <?php
-/* 
+/*
     Plugin Name: Social Media Tracker
    Plugin URI: http://my-awesomeness-emporium.com
-   Description: a plugin to track social media activity of users 
+   Description: a plugin to track social media activity of users
    Version: 1.0
    Author: Survivors
    Jquery Version: 1.7.2
-  
+
 */
 
 //INCLUDES
@@ -21,28 +21,28 @@ function admin_load_js(){
     wp_enqueue_style('social_media_css', plugin_dir_url(__FILE__) . 'assets/css/main.css');
     wp_enqueue_script( 'social_media_js', plugin_dir_url( __FILE__ ) . 'assets/js/script.js',  array('jquery'), '1.7.2' );
     wp_localize_script('social_media_js', 'myAjax', array('ajax_url' => admin_url( 'admin-ajax.php')));
-    
+
 }
 add_action('admin_enqueue_scripts', 'admin_load_js');
 
 function allow_wildcards( $where ) {
     global $wpdb;
     $where = str_replace(
-    "meta_key = 'program_%_name", 
+    "meta_key = 'program_%_name",
     "meta_key LIKE 'program_%_name",
     $wpdb->remove_placeholder_escape($where)
     );
     return $where;
     }
-    
+
     add_filter('posts_where', 'allow_wildcards');
 
     add_action("wp_ajax_hit_my_ajax", "hit_my_ajax");
-    
+
     function hit_my_ajax(){
         $result = array (
             'result' => 'success'
-        ); 
+        );
 
         echo json_encode($result);
         exit();
